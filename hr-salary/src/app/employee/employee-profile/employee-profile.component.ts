cbb56d02febb914458151b0cdcab387ab130e236
uuit <1812890566@qq.com>
2018-04-13 17:54:49 +0800

    kaoqin

--- hr-salary/src/app/employee/employee-profile/employee-profile.component.ts
+++ hr-salary/src/app/employee/employee-profile/employee-profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee-profile',
-  templateUrl: './employee-profile.component.html',
+  templateUrl: './employee-profile.component.pug',
   styleUrls: ['./employee-profile.component.scss']
 })
 export class EmployeeProfileComponent implements OnInit {
--- hr-salary/src/app/route/route.module.ts
+++ hr-salary/src/app/route/route.module.ts
@@ -9,11 +9,13 @@ import {AttendanceMainComponent} from '../attendance/attendance-main/attendance-
 import {Err500Component} from '../error/err500/err500.component';
 import {Err404Component} from '../error/err404/err404.component';
 import {ReportMainComponent} from '../report/report-main/report-main.component';
+import {EmployeeProfileComponent} from '../employee/employee-profile/employee-profile.component';
 
 const routes: Routes = [
   {path: '', component: WorkbenchMainComponent},
   {path: 'employee', component: EmployeeMainComponent},
   {path: 'employee/add', component: EmployeeAddComponent},
+  {path: 'employee/profile/:id', component: EmployeeProfileComponent},
   {path: 'attendance', component: AttendanceMainComponent},
   {path: 'salary', component: SalaryMainComponent},
   {path: 'report', component: ReportMainComponent},
@@ -31,6 +33,7 @@ const routes: Routes = [
     WorkbenchMainComponent,
     EmployeeMainComponent,
     EmployeeAddComponent,
+    EmployeeProfileComponent,
     AttendanceMainComponent,
     SalaryMainComponent,
     ReportMainComponent,
